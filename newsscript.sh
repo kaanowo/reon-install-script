ab6d100e481d15b2bb2f8d2a62a15e60596dc2cd
Kaan <kaan.guersoy@flauschig.net>
2024-10-30 19:40:00 +0100

    Added kabili207s fork of Pokecrystal news to the news machine setup.

--- newsscript.sh
+++ newsscript.sh
@@ -6,3 +6,31 @@ if [ "$(id -u)" -ne "0" ]; then
 fi
 
 set -eo pipefail
+
+
+echo "Building news for the news-machine."
+
+
+cd /var/www/
+
+git clone https://github.com/kabili207/pokecrystal-news-en
+
+cp config.json /var/www/pokecrystal-news-en/
+
+cd pokecrystal-news-en
+
+wget https://github.com/gbdev/rgbds/releases/download/v0.6.0/rgbds-0.6.0.tar.gz
+
+tar xvf rgbds-0.6.0.tar.gz &>>$logfile
+
+cd rgbds/
+
+make install PREFIX=../ &>>$logfile
+
+cd ..
+
+make RGBDS=./bin/ news all &>>$logfile
+
+./deploy.py
+
+echo "News have been added to the database."
